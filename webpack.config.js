9b179e820b9e9e9064ef00252d97f69ce1777615
Arnaud Villani <arnaud.villani@gmail.com>
2016-10-26 14:59:59 +0200

    [CONFIG] fixed fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -38,15 +38,8 @@ module.exports = {
             include: __dirname+"/assets/img"
           },
           {
-              test: /\.(eot|svg|ttf|woff|woff2)$/,
-              loader: 'file?name=assets/fonts/[name].[ext]'
-          },
-          { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=assets/fonts/[name].[ext]' },
-          { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=assets/fonts/[name].[ext]' },
-          { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=assets/fonts/[name].[ext]' },
-          { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=assets/fonts/[name].[ext]' },
-          { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=assets/fonts/[name].[ext]' }
-
+           test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' 
+          }
         ]
      },
   resolve: {
