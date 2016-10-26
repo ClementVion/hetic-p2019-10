5b725be672ed40c4adadb951dcf46f9afd495bf1
Clément Vion <clement.vion1@gmail.com>
2016-10-26 10:39:38 +0200

    [CONFIG]Try to add font with webpack but doesn't work yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: "./entry.js",
@@ -35,7 +36,17 @@ module.exports = {
             test: /\.(jpg|png)$/,
             loader: 'url?limit=25000',
             include: __dirname+"/assets/img"
-          }
+          },
+          {
+              test: /\.(eot|svg|ttf|woff|woff2)$/,
+              loader: 'file?name=assets/fonts/[name].[ext]'
+          },
+          { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=assets/fonts/[name].[ext]' },
+          { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=assets/fonts/[name].[ext]' },
+          { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=assets/fonts/[name].[ext]' },
+          { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=assets/fonts/[name].[ext]' },
+          { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=assets/fonts/[name].[ext]' }
+
         ]
      },
   resolve: {
