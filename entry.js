050cbb9f978efd0c83080f68ab2a3a86f9fd03ab
clarzou <clairezunda@gmail.com>
2016-11-03 09:40:38 +0100

    Revert "Page project"

--- entry.js
+++
@@ -1,4 +0,0 @@
-require('./assets/styles/main.scss');
-require('./assets/html/home.html');
-var content = require('./assets/scripts/content.es6');
-var project = require('./assets/scripts/page-project.es6');
