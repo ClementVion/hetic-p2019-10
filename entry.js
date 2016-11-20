662b37083acbeef9f394ea61e3fcce9b359bfed8
Clément Vion <clement.vion1@gmail.com>
2016-11-20 13:34:40 +0100

    [FEATURE] lazyloading on project page with lazysize.js

--- entry.js
+++ entry.js
@@ -1,6 +1,8 @@
 require('./assets/styles/main.scss');
 
  window._ = require('underscore');
+ var lazyload = require('lazysizes');
  var gsap = require('gsap'),
  project = require('./assets/scripts/page-project.es6'),
  routing = require('./assets/scripts/router.es6');
+ routing = require('./assets/scripts/lazy-loading.es6');
