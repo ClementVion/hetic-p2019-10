5f82c3872f6c0cd68c66552e6c7251a43e5ea7ee
Clément Vion <clement.vion1@gmail.com>
2016-11-23 20:22:46 +0100

    [FIX] trying to build with gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('images', function(){
+  gulp.src('assets/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['assets/styles/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/styles/'))
+});
+
+gulp.task('scripts', function() {
+    browserify({ debug: true })
+        .transform(babelify)
+        .require("/assets/scripts/**/*.es6", { entry: true })
+        .bundle()
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+gulp.task('build', ['styles','scripts','images']);
