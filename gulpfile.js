887231d64a96224ab0b12718d8ea6c3bc30fe787
Clément Vion <clement.vion49@gmail.com>
2016-12-23 00:23:22 +0100

    routes and links

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const replace = require('gulp-replace')
+const clean = require('gulp-clean')
 
 gulp.task('copy-assets-to-dist', () => {
   gulp.src([
@@ -9,17 +10,22 @@ gulp.task('copy-assets-to-dist', () => {
     'assets/img/*',
   ]).pipe(gulp.dest('dist/public/img/'));
   gulp.src([
-    'assets/fonts/*',
-  ]).pipe(gulp.dest('dist/public/fonts/'));
-  gulp.src([
     './bundle.js',
   ]).pipe(gulp.dest('dist/public/'));
+  gulp.src([
+    './index.html',
+  ]).pipe(gulp.dest('dist/public/'));
+  /*gulp.src([
+    'dist/public/*.{woff,eot,ttf}',
+  ]).pipe(gulp.dest('dist/public/fonts/'))
+    .pipe(clean({force: true}));*/
 })
 
 gulp.task('replace-path', () => {
   gulp.src([
     'dist/public/bundle.js',
     'dist/html/*.html',
-  ]).pipe(replace('../../assets/', './'))
+  ]).pipe(replace('../../assets/', '/'))
+    .pipe(replace('../assets/', '/'))
     .pipe(gulp.dest('dist/public/'));
 })
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = createConfig([
         {
             test: /\.json$/,
             loader: "json"
-        },
+        }
       ]
     }
   })
